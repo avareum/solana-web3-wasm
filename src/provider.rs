0951d1960cb4f97e63e97be544c46c0a5141464b
katopz <katopz@gmail.com>
2022-11-21 19:43:14 +0700

    fix: wasm throw error

--- src/provider.rs
+++ src/provider.rs
@@ -1,4 +1,3 @@
-use reqwest::Url;
 use solana_client_wasm::WasmClient;
 use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
 
@@ -10,8 +9,6 @@ pub enum EndPoint {
     Mainnet,
     #[strum(serialize = "https://api.devnet.solana.com")]
     Devnet,
-    #[strum(disabled)]
-    CustomUrl(Url),
 }
 
 pub struct Web3Provider {
@@ -20,13 +17,8 @@ pub struct Web3Provider {
 
 impl Web3Provider {
     pub fn new(endpoint: &EndPoint) -> Self {
-        Web3Provider::new_with_client(WasmClient::new_with_commitment(
-            match endpoint {
-                EndPoint::Mainnet => EndPoint::Mainnet.to_string(),
-                EndPoint::Devnet => EndPoint::Devnet.to_string(),
-                EndPoint::CustomUrl(url) => url.to_string(),
-            }
-            .as_ref(),
+        Self::new_with_client(WasmClient::new_with_commitment(
+            endpoint.to_string().as_ref(),
             CommitmentConfig {
                 commitment: CommitmentLevel::Finalized,
             },
@@ -34,7 +26,7 @@ impl Web3Provider {
     }
 
     pub fn new_with_client(client: WasmClient) -> Web3Provider {
-        Self { client }
+        Web3Provider { client }
     }
 
     pub fn new_mainnet() -> Web3Provider {
@@ -44,27 +36,5 @@ impl Web3Provider {
     #[allow(dead_code)]
     pub fn new_devnet() -> Web3Provider {
         Web3Provider::new(&EndPoint::Devnet)
-    }
-}
-
-#[cfg(test)]
-mod test {
-
-    use reqwest::Url;
-
-    use super::{EndPoint, Web3Provider};
-
-    #[tokio::test]
-    async fn test_new() {
-        let provider = Web3Provider::new_mainnet();
-        assert!(provider.client.get_latest_blockhash().await.is_ok());
-
-        let provider = Web3Provider::new_devnet();
-        assert!(provider.client.get_latest_blockhash().await.is_ok());
-
-        let provider = Web3Provider::new(&EndPoint::CustomUrl(
-            Url::parse("https://api.mainnet-beta.solana.com").unwrap(),
-        ));
-        assert!(provider.client.get_latest_blockhash().await.is_ok());
     }
 }
